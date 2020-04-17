e5a02800b214cfe4595a1447a421ac944869cab3
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-17 11:25:25 -0500

    reducer done

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,8 +1,33 @@
 const initialState = {
 	name: 'Brainey - hard coded',
+	isFetching: false,
 	age: 200,
 	height: '5cm',
 	id: 0,
+	error: '',
 };
-const smurfReducer = (state = initialState, action) => {};
+const smurfReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'FETCH_SMURF_START':
+			return {
+				...state,
+				isFetching: true,
+			};
+		case 'FETCH_SMURF_SUCCESS':
+			return {
+				...state,
+				isFetching: false,
+				name: action.payload,
+				error: '',
+			};
+		case 'FETCH_SMURF_FAILURE':
+			return {
+				...state,
+				isFetching: false,
+				error: action.payload,
+			};
+		default:
+			return state;
+	}
+};
 export default smurfReducer;
